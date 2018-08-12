cc384ca2be914ff761838fb0a5fb057c5f08e391
kimkihwan <kihwan0101@gmail.com>
2018-08-12 18:56:02 +0900

    index url 추가

--- photowall/urls.py
+++ photowall/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^/', include('blog.urls', namespace='blog')),
 ]
 
